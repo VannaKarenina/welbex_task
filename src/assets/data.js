602264e0db2f9f3b2008f50b65aaf50bb3e299b6
VannaKarenina <46137062+VannaKarenina@users.noreply.github.com>
2022-10-15 03:09:36 +0300

    owerwrite blank project

---
+++ src/assets/data.js
@@ -0,0 +1,110 @@
+export const tableDataAsset = [
+    {
+        "date": "2014-11-27",
+        "name": "Angelique Hickman",
+        "amount": 683,
+        "distance": 711
+    },
+    {
+        "date": "2016-06-10",
+        "name": "Mayer Ramos",
+        "amount": 444,
+        "distance": 422
+    },
+    {
+        "date": "2020-02-08",
+        "name": "Rosa Henry",
+        "amount": 848,
+        "distance": 471
+    },
+    {
+        "date": "2019-08-15",
+        "name": "Carolina Parker",
+        "amount": 755,
+        "distance": 306
+    },
+    {
+        "date": "2020-01-23",
+        "name": "Robyn Barr",
+        "amount": 574,
+        "distance": 190
+    },
+    {
+        "date": "2019-07-24",
+        "name": "Soto Steele",
+        "amount": 158,
+        "distance": 830
+    },
+    {
+        "date": "2017-11-11",
+        "name": "Glover Marks",
+        "amount": 814,
+        "distance": 626
+    },
+    {
+        "date": "2014-04-03",
+        "name": "Maryanne Bernard",
+        "amount": 494,
+        "distance": 647
+    },
+    {
+        "date": "2019-12-07",
+        "name": "Nadia Hinton",
+        "amount": 420,
+        "distance": 504
+    },
+    {
+        "date": "2022-01-25",
+        "name": "Kayla Reese",
+        "amount": 961,
+        "distance": 881
+    },
+    {
+        "date": "2014-06-06",
+        "name": "Robertson Norton",
+        "amount": 995,
+        "distance": 693
+    },
+    {
+        "date": "2014-12-19",
+        "name": "Dominique Keith",
+        "amount": 359,
+        "distance": 560
+    },
+    {
+        "date": "2019-09-28",
+        "name": "Angel Beach",
+        "amount": 340,
+        "distance": 873
+    },
+    {
+        "date": "2015-07-28",
+        "name": "May Kane",
+        "amount": 247,
+        "distance": 471
+    },
+    {
+        "date": "2018-01-20",
+        "name": "Jewel Kline",
+        "amount": 815,
+        "distance": 211
+    },
+    {
+        "date": "2022-07-20",
+        "name": "Alyson Pearson",
+        "amount": 803,
+        "distance": 417
+    },
+    {
+        "date": "2022-01-20",
+        "name": "Alyson Casper",
+        "amount": 803,
+        "distance": 209
+    },
+    {
+        "date": "2018-02-08",
+        "name": "Horne Jenkins",
+        "amount": 539,
+        "distance": 522
+    }
+];
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,121 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home__table__container">
+    <div class="row">
+      <div class="col-4">
+        <label for="colSelection" class="form-label">Колонка</label>
+        <select class="form-select" v-model="selectedCol" id="colSelection" aria-label="Default select example">
+          <option value="Date">Date</option>
+          <option value="Name">Name</option>
+          <option value="Amount">Amount</option>
+          <option value="Distance">Distance</option>
+        </select>
+      </div>
+      <div class="col-4">
+        <label for="conditionSelection" class="form-label">Условие</label>
+        <select class="form-select" v-model="selectedCondition" id="conditionSelection" aria-label="Default select example">
+          <option value="=">=</option>
+          <option value=">">></option>
+          <option value="<"><</option>
+          <option value="contain">Contain</option>
+        </select>
+      </div>
+      <div class="col-4">
+        <label for="filterValueField" class="form-label">Значение условия</label>
+        <input type="text" v-model="filterVal" @input="filter()" id="filterValueField" class="form-control" placeholder="Значение">
+      </div>
+    </div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Date</th>
+          <th scope="col" @click="sortData('name'); sortType = !sortType">Name</th>
+          <th scope="col" @click="sortData('amount'); sortType = !sortType">Amount</th>
+          <th scope="col" @click="sortData('distance'); sortType = !sortType">Distance</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in perPageData">
+          <th scope="row">{{item.date}}</th>
+          <td>{{item.name}}</td>
+          <td>{{item.amount}}</td>
+          <td>{{ item.distance }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <ul class="pagination">
+      <li class="page-item" v-for="page in pages">
+        <a class="page-link" @click="changePage(page)">{{page}}</a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import {tableDataAsset} from "@/assets/data";
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      pageNumber: 1,
+      itemsPerPage: 5,
+      tableData: tableDataAsset,
+      sortType: true, // true - descending false - ascending
+      selectedCol: '',
+      selectedCondition: '',
+      filterVal: ''
+    }
+  },
+  computed: {
+    pages() {
+      return (Math.ceil(this.tableData.length / this.itemsPerPage));
+    },
+    perPageData() {
+      let startPosition = this.itemsPerPage * (this.pageNumber - 1);
+      let endPosition = startPosition + this.itemsPerPage;
+      return this.tableData.slice(startPosition, endPosition);
+    }
+  },
+  methods: {
+    changePage(number) {
+      this.pageNumber = number;
+    },
+    sortData(sortBy) {
+      this.tableData.sort((a,b) => {
+        return this.sortType ? this.compareSort(a[sortBy], b[sortBy]) : this.compareSort(b[sortBy], a[sortBy])
+      })
+    },
+    compareSort(a, b) {
+      if(typeof a == 'number') {
+        return a - b;
+      }
+      return a.localeCompare(b)
+    },
+    filter() {
+      const buffer = tableDataAsset;
+      let subLayer = {
+        '=': function (a,b) {
+          return  buffer.filter((item) => {
+            return item[a] == b;
+          })
+        },
+        '>': function (a,b) {
+          return  buffer.filter((item) => {
+            return item[a] > b;
+          })
+        },
+        '<': function (a,b) {
+          return  buffer.filter((item) => {
+            return item[a] < b;
+          })
+        },
+        'contain': function (a,b) {
+          return  buffer.filter((item) => {
+            return item[a].toString().includes(b.toLocaleString());
+          })
+        }
+      }
+      this.tableData = subLayer[this.selectedCondition](this.selectedCol.toLowerCase(), this.filterVal)
+    },
   }
 }
 </script>
